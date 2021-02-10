4703c802d8bde702760888d01528ed0c538591fd
timibadass <omoyenitimi@gmail.com>
2021-02-10 20:24:58 +0100

    add examples of toRef and toRefs

---
+++ src/components/cars-number.vue
@@ -0,0 +1,34 @@
+<template>
+  <p>{{ cars.number }}</p>
+</template>
+
+<script>
+  import { watch, toRefs, toRef } from "vue";
+  export default {
+    props: {
+      cars: {
+        type: Object,
+        required: true,
+      },
+      gender: {
+        type: String,
+        required: true,
+      },
+    },
+    setup(props) {
+      let { cars } = toRefs(props);
+      let gender = toRef(props, "gender");
+      console.log(gender.value);
+
+      watch(
+        () => cars,
+        (cars, prevCars) => {
+          console.log("deep ", cars.value, prevCars.value);
+        },
+        { deep: true }
+      );
+    },
+  };
+</script>
+
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="home">
+    <form @click.prevent="">
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Username</th>
+          <th>email</th>
+          <th>Edit Cars</th>
+          <th>Cars</th>
+          <th>Gender</th>
+        </tr>
+        <tr v-for="user in users" :key="user.id">
+          <td>{{ user.name }}</td>
+          <td>{{ user.username }}</td>
+          <td>{{ user.email }}</td>
+          <td>
+            <input
+              type="number"
+              style="width: 20px;"
+              name="cars"
+              id="cars"
+              v-model.number="user.cars.number"
+            />
+          </td>
+          <td>
+            <cars-number :gender="user.gender" :cars="user.cars" />
+          </td>
+          <td>{{ user.gender }}</td>
+        </tr>
+      </table>
+
+      <p>Total number of cars: {{ getTotalCars }}</p>
+    </form>
+  </div>
+</template>
+
+<script>
+  // @ is an alias to /src
+  import carsNumber from "@/components/cars-number.vue";
+  const axios = require("axios");
+  import { ref } from "vue";
+  export default {
+    name: "Home",
+    data() {
+      return {};
+    },
+    setup() {
+      let users = ref([]);
+      // let user = reactive({});
+      const getUsers = async () => {
+        let { data } = await axios({
+          url: "data.json",
+        });
+
+        users.value = data;
+        return users;
+      };
+      // const getUserById = async () => {
+      //   // user = await
+      // };
+      return {
+        users,
+        getUsers,
+      };
+    },
+    components: {
+      carsNumber,
+    },
+    created() {
+      this.getUsers();
+    },
+    computed: {
+      getTotalCars() {
+        let totalCars = 0;
+        let users = this.users;
+        users.forEach((user) => {
+          totalCars += user.cars.number;
+        });
+        return totalCars;
+      },
+    },
+    methods: {},
+  };
+</script>
